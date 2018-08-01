59c270c794a2100973e15e1c7e477400c7bfc2d1
Li Yi <yli@zendesk.com>
2018-08-01 23:06:08 +0800

    Minor improvement on strings.

--- builders/strings.js
+++ builders/strings.js
@@ -1,15 +1,12 @@
 const T = require('@babel/template').default;
 const t = require('@babel/types');
+const util = require('../util');
 
 // convert <string> elements to js suitable for string extraction
 module.exports = (strings) => {
-	return strings.map(s => {
-		let ast = T(`var ${s.id} = _(VALUE); // ${s.title}`, {
+	return strings.map(s =>
+		util.setLoc(T(`var ${s.id} = _(VALUE); // ${s.title}`, {
 			placeholderPattern: /^[$A-Z]+$/, // no underscore
 			preserveComments: true
-		})({ VALUE: t.stringLiteral(s.value) });
-
-		require('../util').setLoc(ast, s.loc);
-		return ast;
-	});
+		})({ VALUE: t.stringLiteral(s.value) }), s.loc));
 };
